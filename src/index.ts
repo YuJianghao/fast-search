089c5697cf8352514b5782a993cfa405cc31a075
winwin2011 <yjh2011@live.com>
2023-08-09 03:21:10 +0800

    feat: search providers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
 import * as vscode from 'vscode'
+import * as icon from './icon'
 
-function searchGoogle(input: string) {
-  const url = `https://www.google.com/search?q=${encodeURIComponent(input)}`
+interface SearchProvider {
+  icon?: string
+  name: string
+  template: string
+}
+
+const searchProviders: SearchProvider[] = [
+  {
+    icon: icon.google,
+    name: 'Google',
+    template: 'https://www.google.com/search?q={{q}}',
+  },
+  {
+    icon: icon.github,
+    name: 'Github',
+    template: 'https://github.com/search?q={{q}}&ref=opensearch',
+  },
+  {
+    icon: icon.npmjs,
+    name: 'npmjs',
+    template: 'https://www.npmjs.com/search?q={{q}}',
+  },
+]
+
+const ProviderQuickPickItems: SearchItem[] = searchProviders.map((provider) => {
+  return {
+    iconPath: provider.icon ? vscode.Uri.parse(provider.icon) : new vscode.ThemeIcon('browser'),
+    label: provider.name,
+    provider,
+  }
+})
+
+interface SearchItem extends vscode.QuickPickItem {
+  provider: SearchProvider
+}
+
+function search(value: string, provider: SearchProvider) {
+  const url = provider.template.replace(/{{q}}/g, encodeURIComponent(value))
   vscode.env.openExternal(vscode.Uri.parse(url))
 }
 
@@ -9,11 +46,24 @@ export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     'fast-search.search',
     async () => {
-      const input = await vscode.window.showInputBox({
-        placeHolder: 'Search in Google',
+      const quickPick = vscode.window.createQuickPick<SearchItem>()
+      quickPick.placeholder = 'Search in Google or select a provider ...'
+      quickPick.items = ProviderQuickPickItems
+      quickPick.onDidAccept(async () => {
+        const activeSearchItem = quickPick.activeItems[0]
+        if (!activeSearchItem) {
+          search(quickPick.value, searchProviders[0])
+        }
+        else {
+          const value = await vscode.window.showInputBox({
+            placeHolder: `Search in ${activeSearchItem.provider.name}`,
+          })
+          if (value)
+            search(value, activeSearchItem.provider)
+        }
+        quickPick.dispose()
       })
-      if (input)
-        searchGoogle(input)
+      quickPick.show()
     },
   )
 
