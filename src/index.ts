aba4c6587def879e8a1af9564c3129b8c8b07443
winwin2011 <yjh2011@live.com>
2023-08-08 22:22:06 +0800

    feat: mvp

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import { window } from 'vscode'
+import * as vscode from 'vscode'
 
-export function activate() {
-  window.showInformationMessage('Hello')
+function searchGoogle(input: string) {
+  const url = `https://www.google.com/search?q=${encodeURIComponent(input)}`
+  vscode.env.openExternal(vscode.Uri.parse(url))
 }
 
-export function deactivate() {
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    'quick-search.search',
+    async () => {
+      const input = await vscode.window.showInputBox({
+        placeHolder: 'Search in Google',
+      })
+      if (input)
+        searchGoogle(input)
+    },
+  )
 
+  context.subscriptions.push(disposable)
 }
+
+export function deactivate() {}
